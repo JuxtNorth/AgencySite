43eaabf688c674630d49bd1464f11528bf5c7886
North077 <deykiron365@gmail.com>
2024-03-18 23:32:05 +0530

    Initial Commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <main className="h-dvh w-screen grid place-items-center">
+    	<h1 className="text-9xl text-slate-800 font-extrabold">Hello, World!</h1>
+    </main>
+  </React.StrictMode>,
+)
