d222ae56298f9b7dbe88cc1a67cba55db1b632d2
North077 <deykiron365@gmail.com>
2024-03-21 00:21:08 +0530

    Fix typing bug

--- src/components/Carousel/Slide.tsx
+++ src/components/Carousel/Slide.tsx
@@ -1,10 +1,10 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode } from "react";
 
 export interface SlideProps {
 	children: ReactNode;
 }
 
-export const Slide: FC = (props) => {
+export const Slide: FC<SlideProps> = props => {
 	return (
 		<div className="aspect-video w-[90%] min-w-0 flex-[0_0_80%] rounded-2xl bg-rose-100 last:mr-4">
 			{props.children}
--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import { useCallback } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@/icons';
 import { Slide } from './Slide';
