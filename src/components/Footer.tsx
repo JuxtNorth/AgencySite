e4e38c6d1b013b5ccea14e7c033f92231137cfc6
North077 <deykiron365@gmail.com>
2024-03-20 21:59:54 +0530

    Fix layout break issues in Chrome Android

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,10 +8,10 @@ export const Footer: FC = () => {
 		<footer className="flex justify-between p-8 md:p-16">
 			<section className="flex flex-col items-start space-y-2 md:space-y-8">
 				<div className=" md:space-y-4">
-					<h1 className="w-56 text-2xl font-normal md:w-72 md:text-4xl md:text-5xl">
+					<h1 className="w-44 text-xl font-normal md:w-72 md:text-4xl md:text-5xl">
 						Hi-Reach Media
 					</h1>
-					<p className="text-md pb-4 text-left font-light md:text-2xl">
+					<p className="pb-4 text-left text-sm font-light md:text-2xl">
 						Email us at <a href="mailto:foo@bar.com">foo@bar.com</a>{' '}
 					</p>
 					<ul className="flex gap-2">
@@ -43,7 +43,7 @@ export const Footer: FC = () => {
 						<a href="#">Refund Policy</a>
 					</li>
 				</ul>
-				<p className="mx-auto flex items-center gap-1 text-xs md:mx-0 md:text-xl">
+				<p className="mx-auto flex items-center gap-1 whitespace-nowrap text-xs md:mx-0 md:text-xl ">
 					Made with <HeartIcon className="text-rose-500" /> by Kiron Dey
 				</p>
 			</section>
