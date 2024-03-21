c2f3fa7319b2a6ff038470b762ec5e1d6d23c282
North077 <deykiron365@gmail.com>
2024-03-21 23:43:42 +0530

    Add base styling for Membership Perks section

--- src/pages/Home/Sections/MembershipDetails.tsx
+++ src/pages/Home/Sections/MembershipDetails.tsx
@@ -1,5 +1,26 @@
 import { FC } from 'react';
 
+const Card: FC = () => {
+	return (
+		<article className="mx-auto w-64 text-center md:mx-0 md:w-fit">
+			<h1 className="mx-auto w-48 text-3xl font-semibold">Design Board</h1>
+			<p className="text-sm leading-tight">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum error,
+				sed et sit magnam unde.
+			</p>
+		</article>
+	);
+};
+
 export const MembershipDetails: FC = () => {
-	return <section className="h-screen bg-yellow-100"></section>;
+	return (
+		<section className="grid grid-cols-1 grid-rows-6 gap-8 p-8 md:grid-cols-3 md:grid-rows-2">
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+		</section>
+	);
 };
