bc86d3c48fbc2dbb5cc39f970ff047c9c4ed4b35
North077 <deykiron365@gmail.com>
2024-03-21 23:13:09 +0530

    Change Case Study Section

--- src/pages/Home/Sections/CaseStudies.tsx
+++ src/pages/Home/Sections/CaseStudies.tsx
@@ -2,15 +2,23 @@ import { FC } from 'react';
 
 export const CaseStudies: FC = () => {
 	return (
-		<section className="m-4 rounded-2xl bg-pink-200 p-8 pt-16">
-			<h1 className="pb-2 text-3xl font-semibold md:text-5xl">Case Study on</h1>
-			<h2 className="mb-4 text-5xl font-bold text-rose-500 md:mb-8 md:text-7xl">
-				Client Results
-			</h2>
-			<div className="space-y-4 rounded-2xl bg-white p-4 text-2xl md:space-y-8 md:p-8 md:text-4xl">
-				<h1 className="pl-2">Lorem Ipsum →</h1>
-				<hr />
-				<h1 className="pl-2">Doler Imet →</h1>
+		<section className="m-4 rounded-2xl p-8 pt-16 text-center">
+			<h1 className="text-4xl font-extrabold md:text-5xl">Case Study</h1>
+			<h2 className="text-md md:text-lg">on client results</h2>
+			<div className="mx-auto mt-12 w-[90%] space-y-2 md:w-[48%] md:space-y-4">
+				<div className="flex h-12 items-center gap-5 md:gap-8">
+					<div className="h-12 w-12 shrink-0 rounded-full bg-lime-400 md:h-14 md:w-14"></div>
+					<h1 className="whitespace-nowrap text-3xl font-semibold md:text-3xl">
+						Lorem Chaudhary →
+					</h1>
+				</div>
+				<hr className="border-slate-300" />
+				<div className="flex h-12 items-center gap-5 md:gap-8">
+					<div className="h-12 w-12 shrink-0 rounded-full bg-lime-400 md:h-14 md:w-14"></div>
+					<h1 className="whitespace-nowrap text-3xl font-semibold md:text-3xl">
+						Lorem Sharma →
+					</h1>
+				</div>
 			</div>
 		</section>
 	);
