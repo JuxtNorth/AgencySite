beb72d5098cfaa42f4fdeae39dd1ce428d345821
North077 <deykiron365@gmail.com>
2024-03-21 10:30:31 +0530

    Change styling and add autoplay to carousel

--- src/components/Carousel/Slide.tsx
+++ src/components/Carousel/Slide.tsx
@@ -1,12 +1,12 @@
-import { FC, ReactNode } from "react";
+import { FC, ReactNode } from 'react';
 
 export interface SlideProps {
 	children: ReactNode;
 }
 
-export const Slide: FC<SlideProps> = props => {
+export const Slide: FC<SlideProps> = (props) => {
 	return (
-		<div className="aspect-video w-[90%] min-w-0 flex-[0_0_80%] rounded-2xl bg-rose-100 last:mr-4">
+		<div className="aspect-video min-w-0 flex-[0_0_72%] rounded-2xl bg-rose-200 last:mr-9">
 			{props.children}
 		</div>
 	);
--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -2,12 +2,19 @@ import { useCallback } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@/icons';
 import { Slide } from './Slide';
+import Autoplay from 'embla-carousel-autoplay';
+import { EmblaPluginType } from 'embla-carousel';
+
+const plugins: EmblaPluginType[] = [Autoplay({ delay: 2400 })];
 
 export const Carousel = () => {
-	const [emblaRef, emblaApi] = useEmblaCarousel({
-		loop: true,
-		skipSnaps: true
-	});
+	const [emblaRef, emblaApi] = useEmblaCarousel(
+		{
+			loop: true,
+			skipSnaps: true
+		},
+		plugins
+	);
 
 	const scrollPrev = useCallback(() => {
 		if (emblaApi) emblaApi.scrollPrev();
@@ -18,9 +25,9 @@ export const Carousel = () => {
 	}, [emblaApi]);
 
 	return (
-		<section className="w-full space-y-4 overflow-hidden p-8">
-			<div ref={emblaRef}>
-				<div className="flex gap-4">
+		<section className="mx-auto space-y-4 overflow-hidden">
+			<div className="overflow-hidden" ref={emblaRef}>
+				<div className="flex gap-9">
 					<Slide>Slide 1</Slide>
 					<Slide>Slide 2</Slide>
 					<Slide>Slide 3</Slide>
@@ -28,7 +35,7 @@ export const Carousel = () => {
 					<Slide>Slide 5</Slide>
 				</div>
 			</div>
-			<div className="flex gap-4">
+			<div className="ml-4 flex gap-4">
 				<button className="rounded-full bg-rose-500 p-4" onClick={scrollPrev}>
 					<ChevronLeftIcon />
 				</button>
