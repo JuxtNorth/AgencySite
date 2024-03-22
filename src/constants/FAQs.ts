b359c84a485dd2906bb426a34b3960ce9512c320
North077 <deykiron365@gmail.com>
2024-03-22 18:50:17 +0530

    Update FAQ section

--- src/constants/FAQs.ts
+++ src/constants/FAQs.ts
@@ -2,54 +2,44 @@ import { type FAQAccordionProps } from '@/components';
 
 export const FAQs: FAQAccordionProps[] = [
 	{
-		question: "What's the next step after I've made my payment?",
-		answer:
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nulla cumque est error quisquam asperiores ab adipisci'
+		question: `What's the next step after I've made my payment?`,
+		answer: `After payment, we'll begin the onboarding process, providing you with all necessary details. then soon we will start with the content production`
 	},
 	{
-		question: 'How do we communicate?',
-		answer:
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nulla cumque est error quisquam asperiores ab adipisci'
+		question: `How do we communicate?`,
+		answer: `All our communications takes place on Slack. Your brand managers, editors and designers are always present there to answers your concerns`
 	},
 	{
-		question: 'Is there a limit on the number of requests I can make?',
-		answer:
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nulla cumque est error quisquam asperiores ab adipisci'
+		question: `Is there a limit on the number of requests I can make?`,
+		answer: `Upon subscription, you can add as many video edit requests as you need to your queue, all delivered sequentially. With regards to content, Count on us to fill and organize your content calendar effectively.`
 	},
 	{
-		question: 'How the consultation process works?',
-		answer:
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nulla cumque est error quisquam asperiores ab adipisci'
+		question: `How does the consultation process work?`,
+		answer: `We'll have a call each week at a set time. If something comes up and you need to change or cancel, just let us know as soon as you can. The calls usually last about an hour, but it might vary depending on complexity of the topic.`
 	},
 	{
-		question: "What if the content doesn't meet my expectations?",
-		answer:
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nulla cumque est error quisquam asperiores ab adipisci'
+		question: `What if the content doesn't meet my expectations?`,
+		answer: `We revise till satisfaction. On every Monday we will spend 20 minutes together as we go through your content calendar. Same goes for the video edits.`
 	},
 	{
-		question: 'Are custom edits an option?',
-		answer:
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nulla cumque est error quisquam asperiores ab adipisci'
+		question: `Are custom edits an option?`,
+		answer: `Absolutely, just put your request on the slack channel and the manager will take care of it.`
 	},
 	{
-		question: 'Why should I choose your service over assembling my own team?',
-		answer:
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nulla cumque est error quisquam asperiores ab adipisci'
+		question: `Why should I choose your service over assembling my own team?`,
+		answer: `Leading and managing a team is challenging. Ensuring they grasp your brand, strategy, and maintain quality is even tougher and takes too much of your time. With HiReachMedia!, rest assured, we handle it all seamlessly respecting your time and energy, so you can focus on your business without any hassle.`
 	},
 
 	{
-		question: 'Are custom edits an option?',
-		answer:
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nulla cumque est error quisquam asperiores ab adipisci'
+		question: `Do you provide trials to demonstrate your work?`,
+		answer: `Before assigning our team to you, we'll share sample content, video edits, and relevant case studies they've previously crafted. This allows you to gauge their quality and style. While we don't offer trials or create example videos with your content, we guarantee you won't regret our assistance`
 	},
 	{
-		question: "Can you guarantee my brand's growth?",
-		answer:
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nulla cumque est error quisquam asperiores ab adipisci'
+		question: `Can you guarantee my brand's growth?`,
+		answer: `Every successful brand is unique, but they share one common trait - they regularly create excellent content. While we can't promise growth, we guarantee to create remarkable content for your brand. Although, we guarantee to beat your current results with a huge margin`
 	},
 	{
-		question: 'Is there a refund policy if I choose to cancel?',
-		answer:
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem nulla cumque est error quisquam asperiores ab adipisci'
+		question: `Is there a refund policy if I choose to cancel?`,
+		answer: `Due to the high quality nature of the work, there will be no refunds issued. But you can pause your subscription whenever you want.`
 	}
 ];
