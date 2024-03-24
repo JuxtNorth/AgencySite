b643a8178bb86892119102d6794058a34fdf437e
North077 <deykiron365@gmail.com>
2024-03-24 16:41:10 +0530

    Revise FAQ

--- src/components/FAQ/index.tsx
+++ src/components/FAQ/index.tsx
@@ -13,11 +13,7 @@ export interface FAQAccordionProps {
 
 export const FAQAccordion: FC<FAQAccordionProps> = (props) => {
 	return (
-		<Accordion
-			type="single"
-			className="rounded-2xl border bg-yellow-100 px-8 py-snug"
-			collapsible
-		>
+		<Accordion type="single" className="py-tight" collapsible>
 			<AccordionItem className="border-none" value="item-1">
 				<AccordionTrigger className="md:text-2xl">
 					{props.question}
--- src/pages/Home/Sections/Faq.tsx
+++ src/pages/Home/Sections/Faq.tsx
@@ -5,26 +5,32 @@ import Discover from '@/assets/scribbles/Discover.svg';
 
 export const Faq: FC = () => {
 	return (
-		<section className="space-y-4 p-8 px-loose md:grid md:grid-cols-[69%_30%] md:gap-snug">
-			<section>
+		<section className="space-y-4 md:grid md:grid-cols-[70%_30%] md:p-loose">
+			<section className="px-loose md:px-0 md:pr-loose">
 				<div className="mb-loose md:mt-snug">
 					<h1 className="text-center text-4xl font-semibold md:text-left md:text-6xl">
-						Frequently Asked Questions
+						Frequently Asked
+						<br />
+						Questions
 					</h1>
 				</div>
-				<div className="space-y-snug">
+				<div className="space-y-tight">
 					{FAQs.map((props, i) => (
 						<FAQAccordion {...props} key={i} />
 					))}
 				</div>
 			</section>
-			<section>
-				<div className="rounded-2xl bg-yellow-100 p-6">
+			<section className="px-loose pb-loose md:px-0 md:pb-0">
+				<div className="rounded-2xl bg-[#FFFEDA] p-6">
 					<h1 className="mb-2 text-4xl font-semibold">Have more questions?</h1>
 					<p className="text-lg leading-tight">
 						Schedule a 15 minute call to have all your questions addressed.
 					</p>
-					<img src={Discover} alt="Person looking through a telescope" />
+					<img
+						className="mx-auto"
+						src={Discover}
+						alt="Person looking through a telescope"
+					/>
 					<a
 						href="#"
 						className="block flex w-full items-center justify-center rounded-full bg-[#D7C6FF] py-snug font-display text-[1.3rem] font-semibold"
