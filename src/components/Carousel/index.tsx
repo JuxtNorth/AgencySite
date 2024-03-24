b8641dc4e6ee1f8d5928a97bcffb53492f2dd72c
North077 <deykiron365@gmail.com>
2024-03-25 00:03:27 +0530

    Update Testimonials

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva('flex gap-snug', {
 	variants: {
 		buttons: {
 			edge: 'ml-snug',
-			auto: 'ml-loose md:ml-24 lg:ml-52'
+			auto: 'ml-loose md:ml-24 lg:ml-64'
 		}
 	}
 });
