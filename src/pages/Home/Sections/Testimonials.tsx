1fce4991a670a8e35176f5ad66dd2e174e42f069
juxt_north <deykiron365@gmail.com>
2024-03-26 11:46:11 -0700

    Add

--- src/pages/Home/Sections/Testimonials.tsx
+++ src/pages/Home/Sections/Testimonials.tsx
@@ -3,7 +3,7 @@ import { Carousel, TestimonialCard } from '@/components';
 
 export const Testimonials: FC = () => {
 	return (
-		<section className="relative mt-loose min-h-[86vh] space-y-4 overflow-hidden overflow-visible text-center lg:min-h-fit">
+		<section className="relative mt-loose min-h-[86vh] space-y-4 overflow-hidden text-center lg:min-h-fit">
 			<h2 className="font-semibold">Client Reviews</h2>
 			<Carousel buttonXPosition="center">
 				<TestimonialCard />
--- src/pages/Home/Sections/WorksCarousel.tsx
+++ src/pages/Home/Sections/WorksCarousel.tsx
@@ -3,7 +3,7 @@ import { Carousel3D, Video } from '@/components';
 
 export const WorksCarousel: FC = () => {
 	return (
-		<section className="mt-12 text-center">
+		<section className="mt-12 text-center overflow-hidden">
 			<div className="mb-loose xl:mb-16">
 				<h2 className="font-semibold">Our Work</h2>
 				<p>Making your vision a reality</p>
@@ -11,7 +11,7 @@ export const WorksCarousel: FC = () => {
 			<Carousel3D>
 				{[...new Array(10)].map(() => (
 					<Video
-						className="h-[32rem] w-auto rounded-[1rem] object-cover p-[5px]"
+						className="h-[32rem] w-auto rounded-[1rem] object-cover p-[5px] "
 						src="https://ik.imagekit.io/Hireachmedia/OurWork/1.mp4"
 						poster="https://ik.imagekit.io/Hireachmedia/default-image.jpg?updatedAt=1710837663835"
 						autoPlay
