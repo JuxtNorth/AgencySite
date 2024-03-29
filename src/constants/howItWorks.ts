f5d551fd9472192fa37cf1db559cbf8b646f088a
juxt_north <deykiron365@gmail.com>
2024-03-29 21:59:44 +0530

    Update how it works headings

--- src/constants/howItWorks.ts
+++ src/constants/howItWorks.ts
@@ -2,42 +2,42 @@ import { HowItWorksCardProps } from '@/components';
 
 export const howItWorks: Omit<HowItWorksCardProps, 'children'>[] = [
 	{
-		title: 'It begins with knowing your business and making strategy.',
+		title: 'We begin by understanding your business and strategizing.',
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem dicta laboriosam, corporis ipsum culpa delectus voluptate commodi repudiandae distinctio qui repellendus pariatur inventore assumenda, praesentium velit nihil in nesciunt. Sunt.',
 		index: 1,
 		alternate: true
 	},
 	{
-		title: 'We ideate, script and give you hooks.',
+		title: 'We ideate, script and provide you with hooks.',
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem dicta laboriosam, corporis ipsum culpa delectus voluptate commodi repudiandae distinctio qui repellendus pariatur inventore assumenda, praesentium velit nihil in nesciunt. Sunt.',
 		index: 2,
 		alternate: false
 	},
 	{
-		title: 'You shoot, we edit, animate and publish.',
+		title: 'You shoot, and we edit, animate and publish.',
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem dicta laboriosam, corporis ipsum culpa delectus voluptate commodi repudiandae distinctio qui repellendus pariatur inventore assumenda, praesentium velit nihil in nesciunt. Sunt.',
 		index: 3,
 		alternate: true
 	},
 	{
-		title: 'You get traffic and we nurture them to turn them into leads.',
+		title: 'Your content generates traffic, we nurture it and turn it into leads.',
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem dicta laboriosam, corporis ipsum culpa delectus voluptate commodi repudiandae distinctio qui repellendus pariatur inventore assumenda, praesentium velit nihil in nesciunt. Sunt.',
 		index: 4,
 		alternate: false
 	},
 	{
-		title: 'We set up the whole sales setup for automated lead conversions.',
+		title: 'We set up the sales system for automated lead conversions.',
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem dicta laboriosam, corporis ipsum culpa delectus voluptate commodi repudiandae distinctio qui repellendus pariatur inventore assumenda, praesentium velit nihil in nesciunt. Sunt.',
 		index: 5,
 		alternate: true
 	},
 	{
-		title: 'You show leads the way, you close them.',
+		title: 'We guide the leads and you close them.',
 		description:
 			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem dicta laboriosam, corporis ipsum culpa delectus voluptate commodi repudiandae distinctio qui repellendus pariatur inventore assumenda, praesentium velit nihil in nesciunt. Sunt.',
 		index: 6,
