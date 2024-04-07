b1c596e12e35dd2950a72239de07622c5656a05b
juxt_north <deykiron365@gmail.com>
2024-04-08 01:03:33 +0530

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ export const Footer: FC = () => {
 					<h1 className="w-44 text-xl font-normal md:w-72 md:text-4xl">
 						Hi-Reach Media
 					</h1>
-					<p className="pb-snug text-left text-sm font-light md:text-2xl">
+					<p className="pb-snug text-left text-xs font-light md:text-2xl">
 						Email us at <a href="mailto:foo@bar.com">foo@bar.com</a>{' '}
 					</p>
 					<ul className="flex gap-2">
@@ -32,7 +32,7 @@ export const Footer: FC = () => {
 			</section>
 
 			<section className="flex flex-col justify-between pt-1">
-				<ul className="mx-auto w-full space-y-1.5 text-sm md:text-xl">
+				<ul className="mx-auto w-full space-y-1.5 text-xs md:text-xl">
 					<li>
 						<a href="#">Privacy Policy</a>
 					</li>
@@ -43,7 +43,7 @@ export const Footer: FC = () => {
 						<a href="#">Refund Policy</a>
 					</li>
 				</ul>
-				<p className="mx-auto flex items-center gap-1 whitespace-nowrap text-xs md:mx-0 md:text-xl ">
+				<p className="mx-auto flex items-center gap-1 whitespace-nowrap text-xs md:mx-0 md:text-xl">
 					Made with <HeartIcon className="text-rose-500" /> by Kiron Dey
 				</p>
 			</section>
