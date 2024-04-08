316b2e108974d99a763154522e868099ccc07b18
juxt_north <deykiron365@gmail.com>
2024-04-08 20:06:20 +0530

    test commit

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -8,16 +8,17 @@ export interface ServiceCardProps {
 	description: string;
 }
 
-const jumpToPricingSection = () => {
-	setTimeout(() => {
-		const elem = document.getElementById('pricing');
-		if (elem) {
-			elem.scrollIntoView({
-				behavior: 'smooth'
-			});
-		}
-	}, 200);
-};
+
+// const jumpToPricingSection = () => {
+// 	setTimeout(() => {
+// 		const elem = document.getElementById('pricing');
+// 		if (elem) {
+// 			elem.scrollIntoView({
+// 				behavior: 'smooth'
+// 			});
+// 		}
+// 	}, 200);
+// };
 
 export const ServiceCard: FC<ServiceCardProps> = (props) => {
 	return (
@@ -30,7 +31,7 @@ export const ServiceCard: FC<ServiceCardProps> = (props) => {
 			<p className="text-sm font-semibold leading-7 text-[#aaa]">
 				{props.description}
 			</p>
-			<button className="text-display w-fit rounded-full bg-primary px-4 py-2 text-xs font-semibold !mt-6 !mb-6">
+			<button className="text-display !mb-6 !mt-6 w-fit rounded-full bg-primary px-4 py-2 text-xs font-semibold">
 				Learn more <ArrowIcon className="inline -rotate-45 scale-75" />
 			</button>
 		</article>
