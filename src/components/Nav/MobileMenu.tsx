161c4141de533daaf7963dbc9047a4eb7fedc620
juxt_north <deykiron365@gmail.com>
2024-04-13 15:21:09 +0530

    Update mobile nav menu

--- src/components/Nav/MobileMenu.tsx
+++ src/components/Nav/MobileMenu.tsx
@@ -15,14 +15,14 @@ export const MobileMenu: FC<MobileMenuProps> = ({ isOpen, onClose }) => {
 			if (isOpen) {
 				anime({
 					targets: closeBtnRef.current,
-					translateX: [80, 1],
-					rotateZ: ['72deg', '0deg']
+					translateY: [80, 0],
+					rotateZ: ['90deg', '0deg']
 				});
 			} else {
 				anime({
 					targets: closeBtnRef.current,
-					translateX: [0, 80],
-					rotateZ: ['0deg', '72deg']
+					translateY: [0, 80],
+					rotateZ: ['0deg', '90deg']
 				});
 			}
 		}
@@ -31,44 +31,24 @@ export const MobileMenu: FC<MobileMenuProps> = ({ isOpen, onClose }) => {
 	return (
 		<section
 			data-open={isOpen}
-			className="fixed z-[100] flex h-dvh w-screen flex-col justify-between bg-font-primary p-snug transition-[clip-path] duration-500 [clip-path:circle(0px_at_50%_-20px)] before:fixed before:left-0 before:top-0 before:-z-40 before:h-screen before:w-screen before:bg-font-primary before:content-[''] [&[data-open='false']]:delay-150 [&[data-open='true']]:[clip-path:circle(120vmax_at_50%_-20px)]"
+			className="fixed z-[100] flex h-dvh w-screen flex-col justify-between items-center py-12 bg-font-primary p-snug transition-[clip-path] duration-500 [clip-path:circle(0px_at_50%_-20px)] before:fixed before:left-0 before:top-0 before:-z-40 before:h-screen before:w-screen before:bg-font-primary before:content-[''] [&[data-open='false']]:delay-150 [&[data-open='true']]:[clip-path:circle(120vmax_at_50%_-20px)]"
 		>
-			<div>
-				<button
-					ref={closeBtnRef}
-					onClick={onClose}
-					className="float-right rounded-full bg-primary p-3.5 text-lg"
-				>
-					<CrossIcon />
-				</button>
-
-				<menu className="space-y-2 font-display text-5xl font-extrabold text-surface">
-					<li>Review</li>
-					<li>About</li>
-					<li>Our Works</li>
-					<li>Workflow</li>
-					<li>Schedule</li>
-				</menu>
-			</div>
-			<div className="space-y-3">
-				<p className="text-sm text-surface">
-					Email us at{' '}
-					<a className="hover:underline" href="mailto:foo@bar.com">
-						foo@bar.com
-					</a>
-				</p>
-				<ul className="flex gap-2">
-					<li className="rounded-lg bg-surface p-2">
-						<InstagramIcon />
-					</li>
-					<li className="rounded-lg bg-surface p-2">
-						<LinkedInIcon />
-					</li>
-					<li className="rounded-lg bg-surface p-2">
-						<XIcon />
-					</li>
-				</ul>
-			</div>
+			<h1 className="text-center text-4xl font-extrabold text-surface">
+				Hi Reach Media
+			</h1>
+			<menu className="space-y-2 text-center font-display text-4xl font-extrabold text-surface">
+				<li className="py-2 mx-auto px-8 w-fit rounded-full border-slate-500 border">Workflow</li>
+				<li className="py-2 mx-auto px-8 w-fit rounded-full border-slate-500 border">Our Works</li>
+				<li className="py-2 mx-auto px-8 w-fit rounded-full border-slate-500 border">Pricing</li>
+				<li className="py-2 mx-auto px-8 w-fit rounded-full border-slate-500 border">FAQs</li>
+			</menu>
+			<button
+				ref={closeBtnRef}
+				onClick={onClose}
+				className="rounded-full bg-primary p-snug text-xl w-fit"
+			>
+				<CrossIcon />
+			</button>
 		</section>
 	);
 };
