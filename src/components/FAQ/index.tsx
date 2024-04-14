81d3373b684955f1ac5c77c0af6a17f65c6af0c6
juxt_north <deykiron365@gmail.com>
2024-04-14 20:19:45 +0530

    Update FAQ section

--- src/components/FAQ/index.tsx
+++ src/components/FAQ/index.tsx
@@ -13,9 +13,9 @@ export interface FAQAccordionProps {
 
 export const FAQAccordion: FC<FAQAccordionProps> = (props) => {
 	return (
-		<Accordion type="single" className="py-tight" collapsible>
+		<Accordion type="single" className="relative py-tight hover:transition-colors hover:before:bg-white/10 before:content-[''] before:h-full before:w-[calc(100%+3.8rem)] before:rounded-md before:absolute before:top-0 before:left-[50%] before:-translate-x-[50%] before:-z-[1]" collapsible>
 			<AccordionItem className="border-none" value="item-1">
-				<AccordionTrigger className="text-[1.08rem] xl:text-[1.4rem]">
+				<AccordionTrigger className="text-[1.08rem] xl:text-[1.4rem] hover:no-underline font-bold">
 					{props.question}
 				</AccordionTrigger>
 				<AccordionContent className="">{props.answer}</AccordionContent>
