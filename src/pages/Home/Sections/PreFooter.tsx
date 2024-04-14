4b2b6719d5f6f4b4b59c61dffad6725b12f80803
juxt_north <deykiron365@gmail.com>
2024-04-14 20:43:27 +0530

    Emphasize impact start now button

--- src/pages/Home/Sections/PreFooter.tsx
+++ src/pages/Home/Sections/PreFooter.tsx
@@ -3,11 +3,11 @@ import { FC } from 'react';
 
 export const PreFooter: FC = () => {
 	return (
-		<section className="relative mx-auto mt-24 max-w-[100rem] p-loose py-32 text-center">
+		<section className="relative mx-auto mt-24 max-w-[100rem] px-snug lg:px-loose py-32 text-center">
 			<div className="absolute left-0 top-0 -z-[1] size-full [background-image:url('/plus-long.svg')] [background-size:48px_48px]">
 				<div className="size-full bg-[radial-gradient(ellipse_at_center,transparent,rgb(var(--background)))] mix-blend-multiply" />
 			</div>
-			<h1 className="mb-24 text-6xl font-bold">
+			<h1 className="mb-24 text-3xl lg:text-6xl font-bold">
 				Enough of generic videos edited for the sake of editing
 			</h1>
 			<p className="pb-12">
@@ -17,7 +17,7 @@ export const PreFooter: FC = () => {
 				</span>{' '}
 				today.
 			</p>
-			<button className="relative overflow-hidden rounded-2xl px-loose py-snug font-display text-xl outline outline-1 outline-surface before:absolute before:-bottom-5 before:left-[50%] before:-z-[1] before:block before:size-5 before:-translate-x-[50%] before:rounded-full before:bg-primary before:transition-transform before:duration-500 before:content-[''] hover:before:scale-[15]">
+			<button className="relative overflow-hidden rounded-2xl px-loose py-snug font-display text-xl outline outline-1 outline-[#667] before:absolute before:-bottom-5 before:left-[50%] before:-z-[1] before:block before:size-5 before:-translate-x-[50%] before:rounded-full before:bg-primary before:transition-transform before:duration-500 before:content-[''] hover:outline-surface hover:before:scale-[15]">
 				<GlitterIcon className="mr-2 inline text-secondary" />
 				Start now
 			</button>
