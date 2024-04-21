d6b9a5616874937cc8d6ccda29d206775f023d76
juxt_north <deykiron365@gmail.com>
2024-04-21 17:38:40 +0530

    Add schdule event listener

--- src/pages/Home/Sections/ScheduleCalendar.tsx
+++ src/pages/Home/Sections/ScheduleCalendar.tsx
@@ -1,12 +1,15 @@
 import { FC } from 'react';
-import { InlineWidget } from 'react-calendly';
+import { InlineWidget, useCalendlyEventListener } from 'react-calendly';
 
 export const ScheduleCalendar: FC = () => {
+	useCalendlyEventListener({
+		onEventScheduled: (e) => console.log(e.data.payload),
+	});
 	return (
 		<section className="overflow-hidden pt-loose">
 			<div id="schedule" />
 			<InlineWidget
-				url="https://calendly.com/anant-d9-m"
+				url="https://calendly.com/d/cn7w-x63-h2p/one-off-meeting"
 				styles={{
 					minWidth: '320px',
 					height: '630px',
