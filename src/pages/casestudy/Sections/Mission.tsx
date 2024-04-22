887cab270b3399a03f588cc5f475f263d6682d0e
juxt_north <deykiron365@gmail.com>
2024-04-22 14:18:50 +0530

    Update Case Study header size

--- src/pages/casestudy/Sections/Mission.tsx
+++ src/pages/casestudy/Sections/Mission.tsx
@@ -14,7 +14,7 @@ export const Mission: FC<MissionProps> = (props) => {
 	return (
 		<section className="space-y-snug rounded-2xl bg-surface p-loose md:rounded-3xl md:p-[52px] lg:grid lg:grid-cols-[64%_36%]">
 			<article className="mb-loose space-y-snug text-base text-[#ddd] md:space-y-loose md:text-[1.2rem] md:leading-7 lg:mb-0 lg:pr-12">
-				<h1 className="text-4xl font-bold text-blood-red md:text-7xl">
+				<h1 className="text-5xl font-bold text-blood-red md:text-7xl">
 					Mission &<br className="md:hidden" /> Challenge
 				</h1>
 				<p>{props.contentBlock1}</p>
