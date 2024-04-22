70feff27ae668136ff76337e5f3ec0165a1eb29e
juxt_north <deykiron365@gmail.com>
2024-04-22 14:39:24 +0530

    update policy pages

--- src/components/PolicyPage.tsx
+++ src/components/PolicyPage.tsx
@@ -3,10 +3,12 @@ import { FC } from 'react';
 import { Footer } from './Footer';
 import { Nav } from './Nav';
 import { LoadingSpinner } from './ui';
+import HtmlParser from 'react-html-parser';
 
 interface PolicyPageData {
 	title: string;
 	lastUpdated: string;
+	subTitle: string;
 	content: string[];
 }
 
@@ -18,17 +20,21 @@ export const PolicyPage: FC<{ path: string }> = ({ path }) => {
 			<Nav />
 			{data ? (
 				<>
-					<main className="mt-relaxed space-y-loose p-loose lg:mt-loose lg:p-24">
-						<div className="space-y-2">
-							<h1 className="text-4xl font-semibold text-blood-red md:text-5xl">
+					<main className="mt-relaxed space-y-loose p-loose lg:mt-relaxed lg:px-[12vw] lg:py-32">
+						<div className="lg:mb-12">
+							<h1 className="mb-snug text-4xl font-bold md:text-[clamp(2.5rem,4vw,6rem)] lg:mb-loose">
+								<span className="text-blood-red">Hi-Reach Media</span> -{' '}
 								{data?.title}
 							</h1>
-							<p className="text-sm text-muted md:text-base">
-								Last Updated at {data?.lastUpdated}
-							</p>
+							<em className="text-sm text-muted md:text-base">
+								Last Updated on {data?.lastUpdated}
+							</em>
 						</div>
-						<div className="space-y-4 text-[#ddd]">
-							{data?.content.map((text) => <p>{text}</p>)}
+						<p className="!mb-16 text-[1.1rem]">
+							{data?.subTitle}
+						</p>
+						<div className="space-y-4 text-[#ddd] lg:px-12">
+							{data?.content.map((text) => <p>{HtmlParser(text)}</p>)}
 						</div>
 					</main>
 				</>
